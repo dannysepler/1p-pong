66bac83fc3bf7fc97312c37037edcbf6137e5715
dannysepler <dsepler@ufl.edu>
2014-06-29 23:57:54 -0400

    the player now moves up and down

--- scripts/initial.js
+++ scripts/initial.js
@@ -1,44 +1,86 @@
 /* this script will be used to code the game. 
 distributing the elements comes later */
 
-/*$( document ).ready(function() {
-	var canvas = document.getElementById('canvas');
-	var context = canvas.getContext('2d');
-	context.fillStyle = 'blue';
-	context.font = 'bold 16px Arial';
-	context.fillText('Zibri', 100, 100);
+function fillWhite( ctx ) {
+	ctx.fillStyle='white';
+	ctx.fillRect(0,0,300,500);
+}
 
-	$( '#board' ).click(function() {
-		$( this ).html('');
-	});	
+function clear( ctx ) {
+	ctx.clearRect(0,0,300,500);
+	fillWhite( ctx );
+}
 
-});
-*/
 
+/* ~~~~~~~ THE FIRST FUNCTION ~~~~~~~ */
 function start() {
-	var c=document.getElementById("board");
-	var ctx=c.getContext("2d");
+	var c = document.getElementById("board");
+	var ctx = c.getContext("2d");
 
+	// background color
+	fillWhite( ctx );
+
+	/* start message */
 	ctx.font="20px Georgia";
-	ctx.fillText("Click to Start!",20,70);
+	ctx.fillStyle='black';
+	ctx.fillText("Click to Start!",70,70);
 
+	/* line underneath the text */
 	ctx.beginPath();
-    ctx.moveTo(20, 80);
-    ctx.lineTo(140, 80);
+    ctx.moveTo(70, 80);
+    ctx.lineTo(190, 80);
     ctx.stroke();
 
-    blinkText(c, ctx);
+	c.onclick = function() { 
+		clear( ctx );
+		createPlayer();
+		startTheGame();
+	}
 }
- 
-/*
-//function createPlayer() {
+
+function move( gap ) {
+	var p = document.getElementById('player');
+	var margin = p.style.marginTop;
+	margin = margin.slice(0, -2);	
+	margin = parseInt( margin );
+	margin += gap;
+	if (margin < -320 || margin > -80 ) return;
+	p.style.marginTop = margin.toString() + 'px';
+}
+
+function startTheGame() {
+	var c = document.getElementById('board');
+	var ctx = c.getContext('2d');
+
+	document.onkeydown = function() {
+	    switch (window.event.keyCode) {
+	        case 38: // up arrow
+	            move( -10 );
+	            break;
+	        case 40: // down arrow
+	            move( 10 );
+	            break;
+	    }
+	};
+}
+
+function createPlayer() {
+	var c = document.getElementById("board");
+	var ctx = c.getContext("2d");
+	
 	var player = document.createElement('div');
 
-	player.style.width 	= '30px';
-	div.style.height	= '100px';
-	div.style.background= 'red';
-	div.style.color		= 'black';
+	player.style.width 	= '15px';
+	player.style.height	= '70px';
+	player.style.background= 'red';
+	player.style.color		= 'black';
+	player.style.border	= '1px solid black';
+	player.style.position	= 'absolute';
+	player.style.marginLeft = '50px';
+	player.style.marginTop = '-200px';
 
-	canvas.appendChild(div);
-//}
-*/
+	player.id = 'player';
+
+	var w = document.getElementById('wrapper');
+	w.appendChild(player);
+}
