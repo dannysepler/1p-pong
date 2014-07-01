67599566e3d14eae9efe4fbfcd1bcec4a60161b2
dannysepler <dsepler@ufl.edu>
2014-07-01 00:31:34 -0400

    ball now moves

--- scripts/initial.js
+++ scripts/initial.js
@@ -1,6 +1,12 @@
 /* this script will be used to code the game. 
 distributing the elements comes later */
 
+/* FUNCTIONS THAT GENERALLY JUST DO GOOD THINGS */
+
+function toInt( margin ) { return parseInt( margin.slice(0, -2) ); }
+
+function toString( int ) { return int.toString + 'px'; }
+
 function fillWhite( ctx ) {
 	ctx.fillStyle='white';
 	ctx.fillRect(0,0,300,500);
@@ -31,37 +37,64 @@ function start() {
     ctx.lineTo(190, 80);
     ctx.stroke();
 
+    var started = false;
+
 	c.onclick = function() { 
-		clear( ctx );
-		createPlayer();
-		startTheGame();
+		if (!started) {
+			started = true;
+			clear( ctx );
+			createPlayer();
+			createBall();
+			startTheGame();
+		}
 	}
 }
 
-function move( gap ) {
-	var p = document.getElementById('player');
-	var margin = p.style.marginTop;
+function move(ID, gap, direction ) {
+	var p = document.getElementById(ID);
+	if (direction == 'up') var margin = p.style.marginTop;
+	else var margin = p.style.marginLeft;
+
 	margin = margin.slice(0, -2);	
 	margin = parseInt( margin );
 	margin += gap;
-	if (margin < -320 || margin > -80 ) return;
-	p.style.marginTop = margin.toString() + 'px';
+	
+	if (ID == 'player' && (margin < -320 || margin > -80)) return;
+	
+	if  (direction == 'up') p.style.marginTop = margin.toString() + 'px';
+	else p.style.marginLeft = margin.toString() + 'px';
 }
 
 function startTheGame() {
 	var c = document.getElementById('board');
 	var ctx = c.getContext('2d');
 
+	var dx = 5;
+	var dy = 5;
+
 	document.onkeydown = function() {
 	    switch (window.event.keyCode) {
 	        case 38: // up arrow
-	            move( -10 );
+	            move('player', -10, 'up' );
 	            break;
 	        case 40: // down arrow
-	            move( 10 );
+	            move('player', 10, 'up' );
 	            break;
 	    }
 	};
+	
+	setInterval(function() {
+		var bstyle = document.getElementById( 'ball' ).style;
+		var left = toInt( bstyle.marginLeft );
+		var top  = toInt( bstyle.marginTop  );
+
+		if ( left > 300 || left < 100 )   	{ dx *= -1; }
+		if ( top < -200 || top > -100 ) { dy *= -1; }
+
+		move('ball', dy, 'up');
+		move('ball', dx, 'left');
+	}, 50);
+	
 }
 
 function createPlayer() {
@@ -76,11 +109,28 @@ function createPlayer() {
 	player.style.color		= 'black';
 	player.style.border	= '1px solid black';
 	player.style.position	= 'absolute';
-	player.style.marginLeft = '50px';
 	player.style.marginTop = '-200px';
+	player.style.marginLeft = '50px';
 
 	player.id = 'player';
 
-	var w = document.getElementById('wrapper');
-	w.appendChild(player);
+	document.getElementById('wrapper').appendChild(player);
+}
+
+function createBall() {
+	var c = document.getElementById('board');
+	var ctx = c.getContext('2d');
+
+	var ball = document.createElement('div');
+
+	ball.style.width 	 = '15px';
+	ball.style.height	 = '15px';
+	ball.style.background= 'black';
+	ball.style.position  = 'absolute';
+	ball.style.marginLeft= '250px';
+	ball.style.marginTop = '-170px';
+
+	ball.id = 'ball';
+
+	document.getElementById('wrapper').appendChild(ball);
 }
